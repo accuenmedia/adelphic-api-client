3d1470097c1000bc1a0899ea3958df6786c36ec2
jonjharrison <jj2harrison@gmail.com>
2020-05-26 11:39:21 -0700

    Test for ad_order get_by_id

--- adelphicclient/models/ad_order.py
+++ adelphicclient/models/ad_order.py
@@ -8,8 +8,13 @@ class AdOrder(Base):
 
     object = "adorder"
 
+    # TODO: need get/set sitelist, set deals, set exchanges
+
     def find_by_campaign(self, id):
         url = "{0}/{1}/getAdOrdersByCampaign?id={2}".format(self.connection.url, self.object, id)
         response = self.make_request("GET", url)
 
         return self.get_response_list(response)
+
+    def sync_inventory(self, id):
+        pass
--- tests/test_ad_order.py
+++ tests/test_ad_order.py
@@ -3,7 +3,6 @@ import json
 
 import config
 
-
 from adelphicclient.models.ad_order import AdOrder
 from adelphicclient.service.connection import Connection
 
@@ -18,8 +17,19 @@ class AdOrderTestCase(unittest.TestCase):
     def test_get_ad_orders_by_campaign(self):
         ad_order_service = AdOrder(connection=self.connection)
 
-        ad_orders_string = ad_order_service.find_by_campaign(175552)
+        ad_orders_string = ad_order_service.find_by_campaign(198127)
         ad_orders = json.loads(ad_orders_string)
 
         self.assertEqual(ad_orders["response_code"], 200)
-        self.assertEqual(len(ad_orders["data"]), 23)
+        self.assertEqual(len(ad_orders["data"]), 1)
+
+    def test_get_by_id(self):
+        ad_order_service = AdOrder(connection=self.connection)
+
+        url = "getAdOrder?id=1220142"
+
+        ad_order_string = ad_order_service.get_by_id(url)
+        ad_order = json.loads(ad_order_string)
+
+        self.assertEqual(ad_order["response_code"], 200)
+        self.assertEqual(ad_order["data"]["name"], "JH Test Ad Order")
